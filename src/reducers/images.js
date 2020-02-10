9c8075dd28a160485f1c6d635a5d801752fe16d7
Jitgitbit <rowowolf@gmail.com>
2020-02-10 15:04:33 +0100

    starting to make components

--- src/reducers/images.js
+++ src/reducers/images.js
@@ -1,5 +1,9 @@
+import { ALL_IMAGES } from '../actions.js'
+
 export default function (state = [], action = {}) {
   switch (action.type) {
+    case ALL_IMAGES:
+          return action.payload
     default:
       return state
   }
